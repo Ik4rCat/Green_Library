e1b0ff8d2aa5d14cfd8a7c84b888e2d4b4b33c5d
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-05 08:56:20 +0300

    upload empty MAUI project<alt>

---
+++ GLApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GLApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GLApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GLApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "GLApp.Pages")]
