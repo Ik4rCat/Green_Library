a79a9344e59397a32a349afba5f2e7dc8a367816
ALT <119232131+Ik4rCat@users.noreply.github.com>
2025-11-07 10:53:34 +0300

    updbackup

--- GLApp/Models/Content.cs
+++ GLApp/Models/Content.cs
@@ -1,16 +1,48 @@
-using SQLite;
-
 namespace GLApp.Models;
 
 public class Content
 {
-    [PrimaryKey, AutoIncrement]
-    public int ArticleId { get; set; }
+    string titleSample= "sample Title", textSample= "sample Text";
 
-    [MaxLength(100)]
-    public string? IntroText { get; set; }
+    //data part
 
-    public string? FullText { get; set; }
+    private readonly Data data = new Data();
 
-    public string? Language { get; set; }
+    public string GetTitle(int index) => data.GetTitle(index);
+    public string GetText(int index) => data.GetText(index);
+
+    public class Data
+    {
+        private readonly string[] titles = new string[1000];
+        private readonly string[] texts = new string[1000];
+
+        public Data()
+        {
+            InitializeData();
+        }
+
+        private void InitializeData()
+        {
+            titles[0] = """
+            <html>
+            <head></head>
+                <body>
+                    <h1>Абутилон</h1>
+                </body>
+            </html>
+            """;
+            texts[0] = "Комнатное растение абутилон...";
+        }
+
+        public string GetTitle(int index)
+        {
+            return index >= 0 && index < titles.Length ? titles[index] : string.Empty;
+        }
+
+        public string GetText(int index)
+        {
+            return index >= 0 && index < texts.Length ? texts[index] : string.Empty;
+        }
+
+    }
 }
